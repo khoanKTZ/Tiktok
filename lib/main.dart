47e3007ebff25801074bc91689662ef7e9a3c676
lamquang411 <lamquang4112003@gmail.com>
2023-10-16 12:59:42 +0700

    test

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/check_Connection_firebase/check_server.dart';
 
-//aloalo
 void main() {
   FirebaseOptions options = const FirebaseOptions(
     apiKey: 'AIzaSyDec9jFizbrTJFHBXImSRslKIpBhTGpPYg',
